436ff508782ff506654b002071db19b1a9261420
gyuheeyang <93560449+gyuheeyang@users.noreply.github.com>
2022-03-24 10:46:26 +0900

    Add files via upload
    폴리랜드 파일 출력 코드5 todayDate 배열 추가 및 폴리랜드 매출 분석 기능 구현

--- 폴리랜드 매출 분석 기능 구현.cpp
+++ 폴리랜드 매출 분석 기능 구현.cpp
@@ -1,27 +1,37 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include<stdio.h>
+#include<malloc.h>
+
 int main()
 {
-	//저장 되어있는 파일 읽기   	
-	FILE *filePointer = fopen("report1.csv", "r");
-	int count=0 , i; 
-	int todayDate, ticketArr[30],ticketTimeArr[30],ageGroupArr[30],ticketNumberArr[30],totalPriceArr[30],treatmentGroupArr[30] = {0};
+	//   report.csv 불러오기 	
+	FILE *fp = fopen("report.csv", "r");
+	const int  MAX = 100;
+	int count= 0;
 	
-	
-	while (fscanf(filePointer,"%d,%d,%d,%d,%d,%d,%d", &todayDate,&ticketArr[count],&ticketTimeArr[count],&ageGroupArr[count],
+	//문자열 동적 할당 report 첫 줄 7칸 저장 처리 하지 않음 
+	char *strCategory =(char *)malloc(sizeof(char)*MAX);	
+	for(int i = 0;i< 7; i++){
+		fscanf(fp, "%s\t", strCategory);
+	}
+
+	//입력부  
+	int todayDate[MAX], ticketArr[MAX],ticketTimeArr[MAX],ageGroupArr[MAX],ticketNumberArr[MAX],totalPriceArr[MAX],treatmentGroupArr[MAX];
+	while (fscanf(fp,"%d, %d,%d,%d,%d,%d,%d",&todayDate[count], &ticketArr[count],&ticketTimeArr[count],&ageGroupArr[count],
 					&ticketNumberArr[count],&totalPriceArr[count],&treatmentGroupArr[count])!= -1 ){
 					count++;						
 	}
-	
-	
-	for (int i = 0; i < count; i ++){
-			printf("%d,%d,%d,%d,%d,%d,%d\n", todayDate,ticketArr[i],ticketTimeArr[i],ageGroupArr[i],ticketNumberArr[i],
+	fclose(fp);
+
+	//출력부  
+	printf("=====================report.csv=====================\n");
+	printf ("%7s %5s %5s %5s %5s %5s %7s\n", "날짜","권종","시간","연령","수량","금액","우대사항"); 
+	for (int i = 0; i < count; i++){
+			printf("%d %5d %5d %5d %5d %5d %3d\n",todayDate[i],ticketArr[i],ticketTimeArr[i],ageGroupArr[i],ticketNumberArr[i],
 					totalPriceArr[i],treatmentGroupArr[i]);
 	}
+	printf("====================================================\n");
 	
-	
-
-	fclose(filePointer);
 
 	return 0;
 }
